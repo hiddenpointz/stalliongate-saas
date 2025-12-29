4523ab5ce4295a094771782f67fa386d54d113be
hiddenpointz <hiddenpointz@gmail.com>
2025-12-29 09:48:07 +0530

    Create register.js

---
+++ api/register.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  return res.status(200).json({ 
+    success: true,
+    message: 'Customer registered successfully'
+  });
+}
